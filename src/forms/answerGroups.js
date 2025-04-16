cba18f899131af73c782cb4c09016243a19ca4e1
Eleazar Galope <eleazar.galope@gmail.com>
2025-04-16 09:49:39 +0800

    24thb commit, updated answergroups

--- src/forms/answerGroups.js
+++ src/forms/answerGroups.js
@@ -2,7 +2,7 @@ const answerGroups = {
   "Happiness": {
     exact: ["Happiness", "Kasiyahan", "Happy", "Masaya"],
     related: ["Maligaya", "Natutuwa", "Nagagalak", "Cheerful", "Joyful", "Glad", "Yehey", "Yippie", "Merry", "Delighted", "Thrilled", "Satisfied", "Excited"],
-    unrelated: ["Galit", "Suklam", "Naiinis", "Nabubuwisit", "Nabubwisit", "Naiirita", "Annoyed", "Irritated", "Rage", "Hatred", "Malungkot", "Pighati", "Nagluluksa", "Hinagpis", "Nalulumbay", "Naiiyak", "Unhappy", "Forlorn", "Heartbroken", "Gloomy", "Depressed", "Kadiri", "Nasusuya", "Nagsasawa", "Nakakasuka", "Icky", "Repulsed", "Gross", "Yuck", "Ew", "Takot", "Kaba", "Pangamba", "Nakapagpapabagabag", "Natataranta", "Scared", "Afraid", "Anxious", "Fearful", "Panic", "Terrified", "Gulat", "Nabigla", "Pagkamangha", "Nagimbal", "Shocked", "Stunned", "Astonished", "Startled", "Buwisit", "Bwisit"]
+    unrelated: ["Galit", "Suklam", "Naiinis", "Nabubuwisit", "Nabubwisit", "Naiirita", "Annoyed", "Irritated", "Rage", "Hatred", "Malungkot", "Pighati", "Nagluluksa", "Hinagpis", "Nalulumbay", "Naiiyak", "Unhappy", "Forlorn", "Heartbroken", "Gloomy", "Depressed", "Kadiri", "Nasusuya", "Nagsasawa", "Nakakasuka", "Icky", "Repulsed", "Gross", "Yuck", "Ew", "Takot", "Kaba", "Pangamba", "Nakapagpapabagabag", "Natataranta", "Scared", "Afraid", "Anxious", "Fearful", "Panic", "Terrified", "Gulat", "Nabigla", "Pagkamangha", "Nagimbal", "Shocked", "Stunned", "Astonished", "Startled", "Buwisit", "Bwisit", "Nakangiti", "Smile", "Smiling"]
   },
   "Disgust": {
     exact: ["Disgust", "Disgusted", "Kadiri", "Nandidiri"],
@@ -15,13 +15,13 @@ const answerGroups = {
     unrelated: ["Masaya", "Maligaya", "Natutuwa", "Nagagalak", "Cheerful", "Joyful", "Glad", "Yehey", "Yippie", "Merry", "Delighted", "Thrilled", "Satisfied", "Galit", "Suklam", "Naiinis", "Nabubwisit", "Naiirita", "Annoyed", "Irritated", "Rage", "Hatred", "Malungkot", "Pighati", "Nagluluksa", "Hinagpis", "Nalulumbay", "Naiiyak", "Unhappy", "Forlorn", "Heartbroken", "Gloomy", "Depressed", "Kadiri", "Nasusuya", "Nagsasawa", "Nakakasuka", "Icky", "Repulsed", "Gross", "Yuck", "Ew", "Takot", "Kaba", "Pangamba", "Nakapagpapabagabag", "Natataranta", "Scared", "Afraid", "Anxious", "Fearful", "Panic", "Terrified"]
   },
   "Fear": {
-    exact: ["Fear", "Pagkatakot", "Takot", "Natatakot", "Fearful"],
+    exact: ["Fear", "Pagkatakot", "Takot", "Natatakot", "Fearful", "Natakot"],
     related: ["Kaba", "Pangamba", "Nakapagpabagabag", "Natataranta", "Scared", "Afraid", "Anxious", "Panic", "Terrified", "Worried", "Frightened"],
     unrelated: ["Masaya", "Maligaya", "Natutuwa", "Nagagalak", "Cheerful", "Joyful", "Glad", "Yehey", "Yippie", "Merry", "Delighted", "Thrilled", "Satisfied", "Galit", "Suklam", "Naiinis", "Nabubwisit", "Naiirita", "Annoyed", "Irritated", "Rage", "Hatred", "Malungkot", "Pighati", "Nagluluksa", "Hinagpis", "Nalulumbay", "Naiiyak", "Unhappy", "Forlorn", "Heartbroken", "Gloomy", "Depressed", "Kadiri", "Nasusuya", "Nagsasawa", "Nakakasuka", "Icky", "Repulsed", "Gross", "Yuck", "Ew", "Gulat", "Nabigla", "Pagkamangha", "Nagimbal", "Shocked", "Stunned", "Astonished", "Startled", "Surprised", "Surprise"]
   },
   "Anger": {
     exact: ["Anger", "Angry", "Pagkagalit", "Galit"],
-    related: ["Suklam", "Naiinis", "Nabubuwisit", "Nabubwisit", "Naiirita", "Annoyed", "Irritated", "Rage", "Hatred", "Pissed", "Buwisit", "Bwisit", "Mad", "Dismay", "Frustrated", "Scornful"],
+    related: ["Suklam", "Naiinis", "Nabubuwisit", "Nabubwisit", "Naiirita", "Annoyed", "Irritated", "Rage", "Hatred", "Pissed", "Buwisit", "Bwisit", "Mad", "Dismay", "Frustrated", "Scornful", "Upset", "Grumpy"],
     unrelated: ["Masaya", "Maligaya", "Natutuwa", "Nagagalak", "Cheerful", "Joyful", "Glad", "Yehey", "Yippie", "Merry", "Delighted", "Thrilled", "Satisfied", "Malungkot", "Pighati", "Nagluluksa", "Hinagpis", "Nalulumbay", "Naiiyak", "Unhappy", "Forlorn", "Heartbroken", "Gloomy", "Depressed", "Kadiri", "Nasusuya", "Nagsasawa", "Nakakasuka", "Icky", "Repulsed", "Gross", "Yuck", "Ew", "Takot", "Kaba", "Pangamba", "Nakapagpapabagabag", "Natataranta", "Scared", "Afraid", "Anxious", "Fearful", "Panic", "Terrified", "Gulat", "Nabigla", "Pagkamangha", "Nagimbal", "Shocked", "Stunned", "Astonished", "Startled"]
   },
   "Sadness": {
